f8c9d4a88146ff459887cd4d15d25099e97a1bf5
NickoTesla <mykolandwirt@gmail.com>
2023-04-09 11:00:15 +0200

    HW11_first_trial

--- hw11_firsttrial.py
+++ hw11_firsttrial.py
@@ -1,17 +1,17 @@
-from datetime import datetime, date
-from typing import List, Optional
+from collections import UserDict
 
 
 class Field:
-    def __init__(self, value: str):
-        self._value = value
+    def __init__(self, value):
+        self.value = value
 
-    @property
-    def value(self) -> str:
-        return self._value
+    def __str__(self):
+        return str(self.value)
 
-    def __repr__(self):
-        return f"{self.__class__.__name__}({self.value!r})"
+    def validate(self, value):
+        if not value:
+            return False
+        return True
 
 
 class Name(Field):
@@ -19,99 +19,163 @@ class Name(Field):
 
 
 class Phone(Field):
-    def __init__(self, value: str):
-        self._value = self._clean_value(value)
+    def __init__(self, value):
+        super().__init__(value)
+        self.validate(value)
 
-    @staticmethod
-    def _clean_value(value: str) -> str:
-        digits = "".join(filter(str.isdigit, value))
-        if len(digits) != 10:
-            raise ValueError("Invalid phone number")
-        return digits
+    def validate(self, value):
+        super().validate(value)
+        if not value.isdigit() or len(value) != 10:
+            raise ValueError('Invalid phone number')
 
-    @Field.value.setter
-    def value(self, value: str):
-        self._value = self._clean_value(value)
+    def __set__(self, instance, value):
+        try:
+            self.validate(value)
+            self._value = value
+        except ValueError as e:
+            print(e)
 
 
 class Birthday(Field):
-    DATE_FORMAT = "%d.%m.%Y"
+    def __init__(self, value):
+        super().__init__(value)
+        self.validate(value)
 
-    def __init__(self, value: Optional[str] = None):
-        self._value = self._parse_value(value) if value else None
-
-    def _parse_value(self, value: str) -> date:
+    def validate(self, value):
+        super().validate(value)
         try:
-            parsed_date = datetime.strptime(value, self.DATE_FORMAT).date()
+            date.fromisoformat(value)
         except ValueError:
-            raise ValueError("Invalid date format. Use DD.MM.YYYY") from None
+            raise ValueError('Invalid date format')
 
-        if parsed_date > date.today():
-            raise ValueError("Birthday date can't be in the future")
+    def __set__(self, instance, value):
+        try:
+            self.validate(value)
+            self._
 
-        return parsed_date
 
-    @Field.value.setter
-    def value(self, value: str):
-        self._value = self._parse_value(value)
+@property
+def value(self):
+        return self._value
 
-    def days_to_birthday(self) -> Optional[int]:
-        if not self._value:
-            return None
-        next_birthday = self._value.replace(year=date.today().year)
-        if next_birthday < date.today():
-            next_birthday = next_birthday.replace(year=date.today().year + 1)
-        time_to_birthday = next_birthday - date.today()
-        return time_to_birthday.days
+
+@value.setter
+def value(self, value):
+        self.validate(value)
+        self._value = value
 
 
 class Record:
-    def __init__(
-        self,
-        name: Name,
-        phone: Phone,
-        email: Optional[str] = None,
-        birthday: Optional[Birthday] = None,
-    ):
-        self.name = name
-        self.phone = phone
-        self.email = email
+    def __init__(self, name, phone=None, birthday=None):
+        self.name = Name(name)
         self.birthday = birthday
+        self.phones = []
+        if phone:
+            self.add_phone(phone)
 
-    def days_to_birthday(self) -> Optional[int]:
-        if not self.birthday:
+    def add_phone(self, phone):
+        self.phones.append(Phone(phone))
+
+    def remove_phone(self, phone):
+        self.phones = [p for p in self.phones if p.value != phone]
+
+    def edit_phone(self, old_phone, new_phone):
+        for phone in self.phones:
+            if phone.value == old_phone:
+                phone.value = new_phone
+                break
+
+    def days_to_birthday(self):
+       if self.birthday is None:
             return None
-        return self.birthday.days_to_birthday()
+        today = date.today()
+        birthday = self.birthday.replace(year=today.year)
+        if birthday < today:
+            birthday = birthday.replace(year=today.year + 1)
+        delta = birthday - today
+        return delta.days
 
-    def __repr__(self):
-        return (
-            f"{self.__class__.__name__}(name={self.name!r}, "
-            f"phone={self.phone!r}, email={self.email!r}, "
-            f"birthday={self.birthday!r})"
-        )
+class AddressBook(UserDict):
+    def add_record(self, record):
+        self.data[record.name.value.lower()] = record
+
+    def remove_record(self, name):
+        del self.data[name.lower()]
+
+    def edit_record(self, name, record):
+        self.data[name.lower()] = record
 
 
-class AddressBook:
-    def __init__(self, records: Optional[List[Record]] = None):
-        self._records = records or []
+contacts = AddressBook()
 
-    def add_record(self, record: Record):
-        self._records.append(record)
 
-    def delete_record(self, record: Record):
-        self._records.remove(record)
+def input_error(func):
+    def wrapper(*args):
+        try:
+            return func(*args)
+        except KeyError:
+            return "Contact not found"
+        except ValueError:
+            return "Enter name and phone number separated by a space"
+        except IndexError:
+            return "Enter a contact name"
+    return wrapper
 
-    def find_records(self, query: str) -> List[Record]:
-        return [
-            record
-            for record in self._records
-            if query in record.name.value or query in record.phone.value
-        ]
 
-    def iterator(self, page_size: int):
-        current_index = 0
-        while current_index < len(self._records):
-            yield self._records[current_index: current_index + page_size]
-            current_index += page_size
+@input_error
+def add_contact(command):
+    name, phone = command.split()
+    contacts.add_record(Record(name, phone))
+    return f"Contact {name} added"
 
-    def __len__(
+
+@input_error
+def change_contact(command):
+    name, phone = command.split()
+    record = contacts.data[name.lower()]
+    record.edit_phone(phone)
+    contacts.edit_record(name, record)
+    return f"Phone number for {name} changed"
+
+
+@input_error
+def get_phone(command):
+    name = command.lower()
+    record = contacts.data[name]
+    phones = ", ".join(str(phone) for phone in record.phones)
+    return f"Phone number(s) for {name}: {phones}"
+
+
+def show_all():
+    if not contacts:
+        return "No contacts found"
+    result = "Contacts:\n"
+    for record in contacts.values():
+        phones = ", ".join(str(phone) for phone in record.phones)
+        result += f"{record.name}: {phones}\n"
+    return result
+
+
+def main():
+    while True:
+        command = input("Enter command: ")
+        command = command.lower()
+        if command == "hello":
+            print("How can I help you?")
+        elif command.startswith("add"):
+            print(add_contact(command[4:].strip()))
+        elif command.startswith("change"):
+            print(change_contact(command[7:].strip()))
+        elif command.startswith("phone"):
+            print(get_phone(command[6:].strip()))
+        elif command == "show all":
+            print(show_all())
+        elif command in ["good bye", "close", "exit"]:
+            print("Good bye!")
+            break
+        else:
+            print("Unknown command")
+
+
+if __name__ == "__main__":
+    main()
